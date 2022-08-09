1c3e853e0fd23e24f20efac88a7e1d75f94db5c6
Ammar-Exe <104218064+AmmarrBN@users.noreply.github.com>
2022-08-09 22:43:06 +0700

    Add files via upload

---
+++ module.sh
@@ -0,0 +1,18 @@
+pkg install python
+pkg install python2
+pkg install figlet
+pkg install ruby
+pkg install  bash
+gem install lolcat
+pkg install bastet &> //dev/null
+pkg install vitetris &> //dev/null
+pkg install nsnake &> //dev/null
+pkg install moon-buggy &> //dev/null
+pkg install ninvaders &> //dev/null
+echo "Package Already Installed"
+pip install requests colorama bs4
+echo "pip Already Installed"
+sleep 3
+echo "Running Gabut Game......."
+sleep 8
+python game.py
